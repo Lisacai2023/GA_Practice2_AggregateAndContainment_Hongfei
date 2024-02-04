593b4e381401e8226513fb2ed590f7e3d76a7add
Lisa-cai <145958405+Lisa-cai@users.noreply.github.com>
2024-02-04 01:02:07 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace GA_Practice2_AggregateAndContainment_Hongfei
+{
+    internal class Program
+    {
+        //Week3_Practice2_AggregateAndContainment
+        //Hongfei
+        //02/03/2024
+
+        static List<Student> students;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---- Week3_Practice2_AggregateAndContainment ----");
+
+            PopulateDatabases();
+
+
+            Instructor sam = new Instructor("Sam", "Jones");
+            SchoolProgram prog124 = new SchoolProgram ("Programing", "124","J102", sam);
+
+            foreach (Student student in students)
+            {
+                prog124.AddStudent (student);
+            }
+
+            prog124.DisplayStudents();
+
+
+            //prog124.AddStudet("lisa", "Cai", 100, 100);
+            //prog124.AddStudet("Tina", "Wang", 90, 95);
+            //prog124.AddStudet("Eva", "Zhang", 100, 90);
+
+        }
+
+        public static void PopulateDatabases()
+        {
+            students = new List<Student>();
+
+            students.Add(new Student("lisa", "Cai", 100, 100));
+            students.Add(new Student("Tina", "Wang", 90, 95));
+            students.Add(new Student("Eva", "Zhang", 100, 90));
+
+        }
+
+
+
+
+    }
+}
---
+++ SchoolProgram.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_Practice2_AggregateAndContainment_Hongfei
+{
+    public class SchoolProgram
+    {
+        string _className;
+        string _classNumber;
+        string _roomNumber;
+        Instructor _instructor;
+        List<Student> _classRoster;
+
+        public SchoolProgram(string className, string classNumber, string roomNumber, Instructor instructor)
+        {
+            _className=className;
+            _classNumber=classNumber;
+            _roomNumber=roomNumber;
+            _instructor=instructor;
+            _classRoster = new List<Student>();
+
+        }
+
+        public string ClassName { get => _className; set => _className=value; }
+        public string ClassNumber { get => _classNumber; set => _classNumber=value; }
+        public string RoomNumber { get => _roomNumber; set => _roomNumber=value; }
+        public Instructor Instructor { get => _instructor; set => _instructor=value; }
+        public List<Student> ClassRoster { get => _classRoster;}
+
+        public void AddStudent(Student student)
+        {
+            _classRoster.Add(student); 
+        }
+
+        public void DisplayStudents()
+        {
+            foreach(Student student in _classRoster)
+            {
+                Console.WriteLine($"{_className} {_classNumber} - {_roomNumber} - Instructor {_instructor} - Number of student {_classRoster.Count}");
+                Console.WriteLine(Instructor.ToString());
+                Console.WriteLine(student.ToString());
+            }
+        }
+    }
+}
